e6b87a472fd1e4d1dc3e4d0f6b7cb0e4bb68a543
Gleb Svintsytskyi <globusmen0451@gmail.com>
2022-03-16 19:34:45 +0300

    test server

--- server.js
+++ server.js
@@ -2,8 +2,35 @@ let express = require('express');
 
 let app = express();
 
+let users = [
+    {
+        id: 1,
+        username: 'Vova'
+    },
+    {
+        id: 2,
+        username: 'Nicita'
+    },
+    {
+        id: 3,
+        username: 'Svetlana'
+    }
+];
+
 app.get('/', function (req, res) {
     res.send('Hello API');
+})
+
+app.get('/users', function (req, res) {
+    res.send(users);
+})
+
+app.get('/users/:id', function (req, res) {
+    console.log(req.params);
+    let user = users.find(function (user) {
+        return user.id === Number(req.params.id);
+    });
+    res.send(user);
 })
 
 app.listen(3012, function () {
