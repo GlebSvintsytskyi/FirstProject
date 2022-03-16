c0a5a51af3a252447acc1c7967a5b1619e792683
Gleb Svintsytskyi <globusmen0451@gmail.com>
2022-03-17 00:09:06 +0300

    UpDate server skills

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 
 let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let users = [
     {
         id: 1,
-        username: 'Vova'
+        name: 'Vova'
     },
     {
         id: 2,
-        username: 'Nicita'
+        name: 'Nicita'
     },
     {
         id: 3,
-        username: 'Svetlana'
+        name: 'Svetlana'
     }
 ];
 
@@ -31,6 +35,31 @@ app.get('/users/:id', function (req, res) {
         return user.id === Number(req.params.id);
     });
     res.send(user);
+})
+
+app.post('/users', function (req, res) {
+    console.log(req.body);
+    let user = {
+        id: Date.now(),
+        name: req.body.name
+    };
+    users.push(user);
+    res.send(user);
+})
+
+app.put('/users/:id', function (req, res) {
+    let user = users.find(function (user) {
+        return user.id === Number(req.params.id);
+    });
+    user.name = req.body.name;
+    res.sendStatus(200);
+})
+
+app.delete('/users/:id', function (req, res) {
+    users = users.filter(function (user) {
+        return user.id !== Number(req.params.id);
+    });
+    res.sendStatus(200);
 })
 
 app.listen(3012, function () {
