6f7fc1b8cc2606f92f52a3bd0e5e16de830dc459
Gleb Svintsytskyi <globusmen0451@gmail.com>
2022-03-16 18:19:43 +0300

    Start server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 let express = require('express');
 
 let app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello API');
+})
+
+app.listen(3012, function () {
+    console.log('API app started');
+})
